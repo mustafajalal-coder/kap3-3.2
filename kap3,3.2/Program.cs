393fcd0f33a95777510837d90f2455c8a083c5fd
muja07001 <muja07001@E-PW05CGR9>
2024-11-10 14:10:11 +0100

    Add project files.

---
+++ kap3,3.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kap3_3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("´har du gått ut gymnasiet? (j/n);");
+            string gymnasiestatus = Console.ReadLine().ToLower();
+
+            Console.Write("hur gammal är du? ");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (gymnasiestatus == "j" && ålder < 22)
+
+            {
+                Console.WriteLine("vi vill gärna anställa dig ");
+
+
+            }
+            else
+            {
+                Console.WriteLine("vi letar efter någon annan personal just nu, tyvärr");
+            }
+
+
+
+        }
+           
+    }   
+}
